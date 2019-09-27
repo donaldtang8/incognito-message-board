70461dc643b1a36041314a453cfb6789840267a4
turazi <dtang4@stevens.edu>
2019-09-27 16:46:45 -0400

    Added mongo database and fixed js issues

--- app.js
+++ app.js
@@ -2,20 +2,43 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const ejs = require("ejs");
 const _ = require("lodash");
-const aboutContent = "The idea behind INCOGNITO was inspired by the developer's interactions with individuals who suffer from varying degrees of mental health issues, who then wanted to provide a safe space for these individuals to voice their thoughts and opinions without having the fear of being identified while also providing a comfortable environment with other individuals who are also dealing with mental health issues.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-let posts = [];
 const app = express();
+const aboutContent = "The idea behind INCOGNITO was inspired by the developer's interactions with individuals who suffer from varying degrees of mental health issues, who then wanted to provide a safe space for these individuals to voice their thoughts and opinions anonymously, removing the fear and stigma attached with mental health while also providing a comfortable space and environment with other individuals dealing with mental health issues.";
+let posts = [];
 
 app.set('view engine', 'ejs');
-
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/postList", { useNewUrlParser: true, useUnifiedTopology: true });
+
+const postsSchema = new mongoose.Schema({
+  title: String,
+  body: String,
+  date: String,
+  comments: []
+});
+
+const Post = mongoose.model("Post", postsSchema);
+
+const sample = [{
+  title: "This is what a sample post will look like",
+  body: "To begin posting, click the start posting button above!",
+  date: String(new Date())
+}];
+
 app.get("/", function(req, res) {
-  res.render("home", {posts: posts});
+  Post.find({}, function(err, foundPosts) {
+    if (foundPosts.length === 0) {
+      res.render("home", { posts: sample });
+    } else {
+      console.log("Found posts");
+      res.render("home", { posts: foundPosts });
+    }
+  });
 });
 
 app.get("/about/", function(req, res) {
@@ -23,7 +46,7 @@ app.get("/about/", function(req, res) {
 });
 
 app.get("/contact", function(req, res) {
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact");
 });
 
 app.get("/post", function(req, res) {
@@ -31,39 +54,54 @@ app.get("/post", function(req, res) {
 });
 
 app.post("/compose", function(req, res) {
-  const post = {
+  const newPost = new Post({
     title: _.lowerCase(req.body.postTitle),
     body: req.body.postBody,
-    time: new Date(),
+    date: String(new Date()),
     comments: []
-  };
-  posts.push(post);
+  });
+  newPost.save();
   res.redirect("/");
 });
 
 app.get("/posts/:postId", function(req, res) {
-  let post = posts.find(function(elem) {
-    return elem.title.toLowerCase() === _.lowerCase(req.params.postId);
+  const postTitle = _.lowerCase(req.params.postId);
+  Post.findOne({title: postTitle}, function(err, foundPost) {
+    if (!err) {
+      if (!foundPost) {
+        res.redirect("/");
+      } else {
+        res.render("post", {post: foundPost, comments: foundPost.comments});
+      }
+    } else {
+      console.log(err);
+    }
   });
-  if (post === undefined) {
-    res.redirect("/");
-  } else {
-      res.render("post", {post: post, comments: post.comments});
-  }
 });
 
 app.post("/posts/:postId", function(req, res) {
+  const postTitle = _.lowerCase(req.params.postId);
+  console.log(postTitle);
   let comment = {
     author: req.body.commentAuthor,
     body: req.body.commentBody
   }
-  let post = posts.find(function(elem) {
-    if (elem.title.toLowerCase() === _.lowerCase(req.params.postId)) {
-      elem.comments.push(comment);
-      return elem;
-    }
+  Post.findOne({title: postTitle}, function(err, foundPost) {
+    if (!err) {
+      if (foundPost) {
+        console.log("Found post");
+        foundPost.comments.push(comment);
+        foundPost.save();
+        res.redirect("/posts/" + req.params.postId);
+      }
+    } else {
+        console.log(err);
+      }
   });
-  res.redirect("/posts/" + req.params.postId);
+});
+
+app.get('*', function(req, res){
+  res.render("pageNotFound");
 });
 
 app.listen(3000, function() {
